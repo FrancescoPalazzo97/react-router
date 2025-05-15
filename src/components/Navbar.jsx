f33d98a4b8c7a83270df7a9978043ffdfebbbd51
FrancescoPalazzo97 <francescopalazzo97@gmail.com>
2025-05-15 15:08:07 +0200

    [FEAT] Add navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,19 +3,26 @@ import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
     return (
-        <nav>
-            <ul>
-                <li>
-                    <NavLink to="/">Home</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/about">About</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/items">Items</NavLink>
-                </li>
-            </ul>
-        </nav>
+        <>
+            <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
+                <div className="container-fluid">
+                    <img src="/react.svg" alt="Logo" width="30" height="24" className="d-inline-block align-text-top" />
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <NavLink to="/" className="nav-link" aria-current="page">Home</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/about" className="nav-link">About</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/items" className="nav-link">Items</NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </>
     )
 }
 
