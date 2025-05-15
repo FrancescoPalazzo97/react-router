d6dbc97ae615f2e27b755b76de6e164d7b867c9a
FrancescoPalazzo97 <francescopalazzo97@gmail.com>
2025-05-15 16:30:44 +0200

    [FEAT] Create Card component and add loading indicator during product fetch

--- src/pages/Items.jsx
+++ src/pages/Items.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
+import Card from '../components/Card'
 
 const Items = () => {
 
-    const [items, setItems] = useState([]);
+    const [items, setItems] = useState(null);
 
     useEffect(() => {
         axios.get('https://fakestoreapi.com/products')
@@ -23,20 +24,19 @@ const Items = () => {
                 <div className="row">
                     <div className="col-12">
                         <h1 className='text-center py-4 text-uppercase'>Item List</h1>
-                        <div className="row row-cols-lg-4 row-cols-md-2 row-cols-sm-1 g-5">
-                            {items.map((item) => (
-                                <div className="col d-flex align-items-stretch" key={item.id}>
-                                    <div className="card border-0">
-                                        <img src={item.image} className="card-img-top h-50" alt={item.title} />
-                                        <div className="card-body d-flex flex-column justify-content-between ">
-                                            <h5 className="card-title ">{item.title}</h5>
-                                            <p className="card-text text-truncate">{item.description}</p>
-                                            <p className="card-text ">${item.price}</p>
-                                            <p className="card-text ">{item.category}</p>
-                                        </div>
+                        <div className="row g-5">
+                            {
+                                items === null ? (
+                                    <div className="col-12">
+                                        <span class="loader"></span>
                                     </div>
-                                </div>
-                            ))}
+                                ) : (
+                                    items.map((item) => (
+                                        <Card item={item} />
+                                    ))
+                                )
+                            }
+
                         </div>
                     </div>
                 </div>
