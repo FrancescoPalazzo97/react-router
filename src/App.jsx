59cd3fc6fb96f8866616bf565837220ddaee9aa2
FrancescoPalazzo97 <francescopalazzo97@gmail.com>
2025-05-15 14:42:09 +0200

    [UPD] Create required components and import dependencies from react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/Home.jsx'
+import About from './pages/About'
+import Items from './pages/Items'
 
 function App() {
 
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' Component={Home} />
+        <Route path='/about' Component={About} />
+        <Route path='/items' Component={Items} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
