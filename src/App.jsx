0a466b4637366f793a2185cde11841946839990c
FrancescoPalazzo97 <francescopalazzo97@gmail.com>
2025-05-16 14:47:50 +0200

    [UPD] Center navbar using layout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
 import Home from './pages/Home.jsx'
 import About from './pages/About'
 import Items from './pages/products/Items'
 import Item from './pages/products/Item'
+import Layout from './layouts/Layout.jsx'
 
 function App() {
 
   return (
     <BrowserRouter>
-      <Navbar />
       <Routes>
-        <Route path='/' Component={Home} />
-        <Route path='/about' Component={About} />
-        <Route path='/items'>
-          <Route index Component={Items} />
-          <Route path=':id' Component={Item} />
+        <Route Component={Layout}>
+          <Route path='/' Component={Home} />
+          <Route path='/about' Component={About} />
+          <Route path='/items'>
+            <Route index Component={Items} />
+            <Route path=':id' Component={Item} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
